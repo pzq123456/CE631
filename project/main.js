ab98fb4f76a6aa47d8de09a59fb475d6b4d43df8
潘志清 <82391775+pzq123456@users.noreply.github.com>
2024-11-06 23:49:40 +0800

    更新主面板，添加字体大小动态绑定功能；新增Canvas类以支持多层画布管理

--- project/main.js
+++ project/main.js
@@ -3,18 +3,18 @@ import {Pane} from 'https://cdn.jsdelivr.net/npm/tweakpane@4.0.5/dist/tweakpane.
 const panel = document.getElementsByClassName('panel')[0];
 
 const PARAMS = {
-    factor: 123,
+    fontSize: 16,
     title: 'hello',
     color: '#ff0055',
 };
 
-const pane = new Pane(
-    {
-        container: panel,
-    }
-);
+const pane = new Pane();
 
-pane.addBinding(PARAMS, 'factor');
+pane.addBinding(PARAMS, 'fontSize', {
+    min: 10,
+    max: 100,
+});
+
 pane.addBinding(PARAMS, 'title');
 pane.addBinding(PARAMS, 'color');
 
@@ -22,6 +22,14 @@ pane.addBinding(PARAMS, 'color');
 const h1 = document.createElement('h1');
 h1.textContent = PARAMS.title;
 h1.style.color = PARAMS.color;
+h1.style.fontSize = `${PARAMS.fontSize}px`;
 document.body.appendChild(h1);
 
+function update() {
+    h1.style.fontSize = `${PARAMS.fontSize}px`;
+    h1.textContent = PARAMS.title;
+    h1.style.color = PARAMS.color;
+}
+
+pane.on('change', update);
 
---
+++ project/src/canvas.js
@@ -0,0 +1,130 @@
+/**
+ * this class will create and maintain two canvas #layers (exactally vertial in Z axis) like:
+ * root\ zIndexFrom = 0 (e.g.)
+ *  game Layer\ zIndex = 1
+ *  text Layer\ zIndex = 2
+ *  control Layer\ zIndex = 3
+ */
+import { uuid } from './utils.js';
+
+export class Canvas{
+    #uuid;
+    #width;
+    #height;
+    #layers;
+    #parentElement;
+
+    constructor(
+        parentElement = null,
+        layerNames = ["game", "text", "control"],
+        width = 1024, 
+        height = 512, 
+        zIndexFrom = 0 
+    ){
+
+        this.#uuid = uuid();
+        this.#width = width;
+        this.#height = height;
+
+        this.#parentElement = parentElement;
+        this.zIndexFrom = zIndexFrom;
+
+        this.root = null;
+        this.#layers = null;
+
+        this.#createRoot();
+        this.#createCanvas(layerNames);
+        this.#appendRoot();
+    }
+
+    getCtx(name){
+        return this.#layers[name].getContext('2d');
+    }
+
+    getLayer(name){
+        return this.#layers[name];
+    }
+
+    get ControlLayer(){
+        if(this.#layers["control"]){
+            return this.#layers["control"];
+        }else{
+            let keys = Object.keys(this.#layers);
+            return this.#layers[keys[keys.length - 1]];
+        }
+    }
+
+    get uuid(){
+        return this.#uuid;
+    }
+
+    get width(){
+        return this.#width;
+    }
+
+    get height(){
+        return this.#height;
+    }
+
+    /**
+     * @param {HTMLElement} newParent
+     */
+    set parentElement(newParent){
+        // 然后重新设置父元素
+        this.#parentElement = newParent;
+        this.#appendRoot();
+    }
+
+    #createRoot(){
+        this.root = document.createElement('div');
+        this.root.id = "layer-"+this.uuid;
+
+        let style = {
+            width: this.width + 'px',
+            height: this.height + 'px',
+            position: 'relative',
+            zIndex: this.zIndexFrom,
+            backgroundColor: '#ACD3DF',
+            // backgroundColor: 'black',
+
+        }
+
+        for(let key in style){
+            this.root.style[key] = style[key];
+        }
+    }
+
+    #appendRoot(){
+        if(this.#parentElement){
+            this.#parentElement.appendChild(this.root);
+        }else{
+            document.body.appendChild(this.root);
+        }
+    }
+
+    #createCanvas(layerNames = ["game", "text", "control"]){
+        let canvasLayerNames = layerNames;
+        let zIndex = this.zIndexFrom + 1;
+        this.#layers = {};
+        for(let i = 0; i < canvasLayerNames.length; i++){
+            let name = canvasLayerNames[i];
+            let layer = this.#createCanvasLayer(name, zIndex);
+            this.#layers[name] = layer;
+            this.root.appendChild(layer);
+            zIndex++;
+        }
+    }
+
+    #createCanvasLayer(name, z){
+        let myCanvas = document.createElement('canvas');
+        myCanvas.width = this.width;
+        myCanvas.height = this.height;
+        myCanvas.id = name + "-" + this.uuid;
+        myCanvas.style.position = 'absolute';
+
+        // myCanvas.style.border = '1px solid black';
+        myCanvas.style.zIndex = z;
+
+        return myCanvas;
+    }
+}
